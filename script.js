785beba031cbb3e8b23e3e058850dde0047404dd
Shaker Kattoula <smkattoula@gmail.com>
2020-07-30 14:14:41 -0700

    add infinite scrolling functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById("posts-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Create a function to fetch posts from API
@@ -26,12 +26,34 @@ async function showPosts() {
     postEL.innerHTML = `<div class="number">${post.id}</div>
       <div class="post-info">
         <h2 class="post-title">${post.title}</h2>
-        <p class="post-body>${post.body}></p>
+        <p class="post-body">${post.body}></p>
       </div>`;
 
     postsContainer.appendChild(postEL);
   });
 }
 
+// Show the loader and fetch more posts
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 150);
+  }, 1000);
+}
+
 // Show the inital posts
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
