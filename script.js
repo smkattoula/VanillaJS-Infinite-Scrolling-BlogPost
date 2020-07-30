27a06ba03b2e35d847eef749c52b6cdd3f6cc11a
Shaker Kattoula <smkattoula@gmail.com>
2020-07-30 14:21:45 -0700

    add filter functionality for the input

--- script.js
+++ script.js
@@ -47,6 +47,23 @@ function showLoading() {
   }, 1000);
 }
 
+// Create a function that filters posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
 // Show the inital posts
 showPosts();
 
@@ -57,3 +74,5 @@ window.addEventListener("scroll", () => {
     showLoading();
   }
 });
+
+filter.addEventListener("input", filterPosts);
